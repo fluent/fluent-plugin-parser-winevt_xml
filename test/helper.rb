414aff9e5e49fcb6dd91823fa7e92aae7de58730
Hiroshi Hatake <hatake@clear-code.com>
2019-10-09 19:14:54 +0900

    Import
    Signed-off-by: Hiroshi Hatake <hatake@clear-code.com>

---
+++ test/helper.rb
@@ -0,0 +1,23 @@
+require 'rubygems'
+require 'bundler'
+begin
+  Bundler.setup(:default, :development)
+rescue Bundler::BundlerError => e
+  $stderr.puts e.message
+  $stderr.puts "Run `bundle install` to install missing gems"
+  exit e.status_code
+end
+require 'test/unit'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'fluent/test'
+
+require 'fluent/test/driver/parser'
+require 'fluent/plugin/parser_winevt_xml'
+
+class Test::Unit::TestCase
+end
+require 'fluent/test/helpers'
+
+include Fluent::Test::Helpers
