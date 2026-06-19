a10d7bd42ce4c78aa88f76097a6994f7b0bb62ae
Takuro Ashie <ashie@clear-code.com>
2026-06-19 10:16:23 +0900

    Add tests for parser (:auto, :nokogiri, and :rexml)
    Signed-off-by: Takuro Ashie <ashie@clear-code.com>

--- test/plugin/test_parser_winevt_sax.rb
+++ test/plugin/test_parser_winevt_sax.rb
@@ -2,43 +2,56 @@ require_relative '../helper'
 
 class WinevtSAXparserTest < Test::Unit::TestCase
 
-  def setup
-    Fluent::Test.setup
-  end
-
   CONFIG = %[]
-  XMLLOG = File.open(File.join(__dir__, "..", "data", "eventlog.xml") )
 
   def create_driver(conf = CONFIG)
     Fluent::Test::Driver::Parser.new(Fluent::Plugin::WinevtSAXparser).configure(conf)
   end
 
-  def test_parse
-    d = create_driver
-    xml = XMLLOG
-    expected = {"ProviderName"      => "Microsoft-Windows-Security-Auditing",
-                "ProviderGUID"      => "{54849625-5478-4994-A5BA-3E3B0328C30D}",
-                "EventID"           => "4624",
-                "Qualifiers"        => nil,
-                "Level"             => "0",
-                "Task"              => "12544",
-                "Opcode"            => "0",
-                "Keywords"          => "0x8020000000000000",
-                "TimeCreated"       => "2019-06-13T09:21:23.345889600Z",
-                "EventRecordID"     => "80688",
-                "ActivityID"        => "{587F0743-1F71-0006-5007-7F58711FD501}",
-                "RelatedActivityID" => nil,
-                "ProcessID"         => "912",
-                "ThreadID"          => "24708",
-                "Channel"           => "Security",
-                "Computer"          => "Fluentd-Developing-Windows",
-                "UserID"            => nil,
-                "Version"           => "2",}
-    d.instance.parse(xml) do |time, record|
-      assert_equal(expected, record)
+  class ParseTest < self
+    def setup
+      Fluent::Test.setup
+      @xml = File.open(File.join(__dir__, "..", "data", "eventlog.xml"))
     end
 
-    assert_true(d.instance.winevt_xml?)
+    def teardown
+      @xml.close
+    end
+
+    data(
+      "auto" => [%[parser auto], true],
+      "nokogiri" => [%[parser nokogiri], true],
+      "rexml" => [%[parser rexml], false]
+    )
+    def test_parse(data)
+      config, expected_use_nokogiri = data
+      d = create_driver(CONFIG + config)
+      @xml = File.open(File.join(__dir__, "..", "data", "eventlog.xml"))
+      expected = {"ProviderName"      => "Microsoft-Windows-Security-Auditing",
+                  "ProviderGUID"      => "{54849625-5478-4994-A5BA-3E3B0328C30D}",
+                  "EventID"           => "4624",
+                  "Qualifiers"        => nil,
+                  "Level"             => "0",
+                  "Task"              => "12544",
+                  "Opcode"            => "0",
+                  "Keywords"          => "0x8020000000000000",
+                  "TimeCreated"       => "2019-06-13T09:21:23.345889600Z",
+                  "EventRecordID"     => "80688",
+                  "ActivityID"        => "{587F0743-1F71-0006-5007-7F58711FD501}",
+                  "RelatedActivityID" => nil,
+                  "ProcessID"         => "912",
+                  "ThreadID"          => "24708",
+                  "Channel"           => "Security",
+                  "Computer"          => "Fluentd-Developing-Windows",
+                  "UserID"            => nil,
+                  "Version"           => "2",}
+      d.instance.parse(@xml) do |time, record|
+        assert_equal(expected, record)
+      end
+
+      assert_true(d.instance.winevt_xml?)
+      assert_equal(expected_use_nokogiri, d.instance.instance_variable_get(:@use_nokogiri))
+    end
   end
 
   class QualifiersTest < self
@@ -50,8 +63,14 @@ class WinevtSAXparserTest < Test::Unit::TestCase
       @xml.close
     end
 
-    def test_parse_without_qualifiers
-      d = create_driver CONFIG + %[preserve_qualifiers false]
+    data(
+      "auto" => [%[parser auto], true],
+      "nokogiri" => [%[parser nokogiri], true],
+      "rexml" => [%[parser rexml], false]
+    )
+    def test_parse_without_qualifiers(data)
+      config, expected_use_nokogiri = data
+      d = create_driver(CONFIG + config + %[\npreserve_qualifiers false])
       expected = {"ActivityID"        => nil,
                   "Channel"           => "Application",
                   "Computer"          => "DESKTOP-G457RDR",
@@ -74,6 +93,7 @@ class WinevtSAXparserTest < Test::Unit::TestCase
       end
 
       assert_true(d.instance.winevt_xml?)
+      assert_equal(expected_use_nokogiri, d.instance.instance_variable_get(:@use_nokogiri))
     end
   end
 end
